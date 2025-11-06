91e81c0bfd9cc6af68d07bbbda5b2c1920d1e535
huseyinerenceyhan <120252566+huseyinerenceyhan@users.noreply.github.com>
2025-11-06 17:02:44 +0300

    Update pong.c

--- source/pong.c
+++ source/pong.c
@@ -1,11 +1,18 @@
 #include <tonc.h>
 #include "printdebug.h"
+#include "point.h"
 #include "player.h"
+#include "ball.h"
 #include "player_sprite.h"
 #include "ai_sprite.h"
+#include "vector.h"
 #include "ball_sprite.h"
 #include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
 OBJ_ATTR obj_buffer[128];
+
 
 /* I CAN'T MIX MODES FOR NOW :(
 // I GUESS ITS NOT POSSIBLE??
@@ -32,14 +39,14 @@ void init_sprites(){
 	memcpy16(&pal_obj_mem[16 * 2], ball_spritePal, ball_spritePalLen/ sizeof(u16));
 }
 
-void move_player(player *p){
+void move_human(player *p){
 	
 	if(key_held(KEY_UP)){
 		p->speed += p->acc;
 		if(p->speed > p->cap){
 			p->speed = p->cap;
 		}
-		p->y -= p->speed;
+		p->coordinate->y -= p->speed;
 		u16 new_attr2_value = ATTR2_ID(2);
 		p->sprite->attr2 = (p->sprite->attr2 & ~ATTR2_ID_MASK) | new_attr2_value;
 		
@@ -49,7 +56,7 @@ void move_player(player *p){
 		if(p->speed > p->cap){
 			p->speed = p->cap;
 		}
-		p->y += p->speed;
+		p->coordinate->y += p->speed;
 		
 		u16 new_attr2_value = ATTR2_ID(0);
 		p->sprite->attr2 = (p->sprite->attr2 & ~ATTR2_ID_MASK) | new_attr2_value;
@@ -62,71 +69,232 @@ void move_player(player *p){
 	
 	//CHECKING OUT OF BOUNDS
 	// 16 is sprite size
-	if(p->y<0){
-		p->y=0;
+	if(p->coordinate->y<0){
+		p->coordinate->y=0;
 	}
-	else if((p->y+16)>160){
-		p->y=144;
+	else if((p->coordinate->y+16)>160){
+		p->coordinate->y=144;
 	}
 	
 	
 	
-	obj_set_pos(p->sprite, p->x, p->y);
+	obj_set_pos(p->sprite, p->coordinate->x, p->coordinate->y);
+	
+	
+	
+}
+void move_ai(player *p, ball *b){
+	
+	if(b->corners[BOTTOM_RIGHT].y <= p->corners[TOP_LEFT].y){
+		p->speed += p->acc;
+		if(p->speed > p->cap){
+			p->speed = p->cap;
+		}
+		p->coordinate->y -= p->speed;
+		
+	}
+	else if(b->corners[TOP_RIGHT].y >= p->corners[BOTTOM_LEFT].y){
+		p->speed += p->acc;
+		if(p->speed > p->cap){
+			p->speed = p->cap;
+		}
+		
+		p->coordinate->y += p->speed;
+		
+	}
+	else if(b->corners[TOP_RIGHT].y >= p->corners[TOP_RIGHT].y && b->corners[BOTTOM_RIGHT].y <= p->corners[BOTTOM_RIGHT].y ){
+		p->speed -= p->dec;
+	}
+	
+	
+	//CHECKING OUT OF BOUNDS
+	// 16 is sprite size
+	if(p->coordinate->y<0){
+		p->coordinate->y=0;
+	}
+	else if((p->coordinate->y+16)>160){
+		p->coordinate->y=144;
+	}
+	
+	
+	
+	obj_set_pos(p->sprite, p->coordinate->x, p->coordinate->y);
 	
 	
 	
 }
 
+void move_ball(ball *b, vector *v){
+	//TOP BORDER CHECK
+	if(b->coordinate->y < 0){
+		v->y *= -1;
+	}
+	//BOTTOM BORDER CHECK
+	else if((b->coordinate->y+8	) > SCREEN_HEIGHT){
+		v->y *= -1;
+	}
+	b->coordinate->x += v->x;
+	b->coordinate->y += v->y;
+
+	obj_set_pos(b->sprite, b->coordinate->x, b->coordinate->y);
+	
+}
+
+void check_collision_human(ball *b, player *p, vector *v){
+
+		// SAME X CHECK
+	if(p->corners[TOP_RIGHT].x == b->corners[TOP_LEFT].x){
+		
+	}
+	else{
+		return;
+	}
+	
+	
+	if(b->corners[TOP_LEFT].y <= p->corners[TOP_RIGHT].y && b->corners[BOTTOM_LEFT].y >= p->corners[TOP_RIGHT].y ){
+		//partial top
+	}
+	else if(b->corners[TOP_LEFT].y >= p->corners[TOP_RIGHT].y && b->corners[BOTTOM_LEFT].y <= p->corners[BOTTOM_RIGHT].y){
+		//middle
+	}
+	else if(b->corners[TOP_LEFT].y <= p->corners[BOTTOM_RIGHT].y && b->corners[BOTTOM_LEFT].y >= p->corners[BOTTOM_RIGHT].y){
+		//partial down
+	}
+	else{
+		return;
+	}
+	
+	v->x *= -1; 
+
+
+
+}
+
+
+void check_collision_ai(ball *b, player *p, vector *v){
+
+		// SAME X CHECK
+	if(p->corners[TOP_LEFT].x == b->corners[TOP_RIGHT].x){
+		
+	}
+	else{
+		return;
+	}
+	
+	
+	if(b->corners[TOP_RIGHT].y <= p->corners[TOP_LEFT].y && b->corners[BOTTOM_RIGHT].y >= p->corners[TOP_LEFT].y ){
+		//partial top
+	}
+	else if(b->corners[TOP_RIGHT].y >= p->corners[TOP_LEFT].y && b->corners[BOTTOM_RIGHT].y <= p->corners[BOTTOM_LEFT].y){
+		//middle
+	}
+	else if(b->corners[TOP_RIGHT].y <= p->corners[BOTTOM_LEFT].y && b->corners[BOTTOM_RIGHT].y >= p->corners[BOTTOM_LEFT].y){
+		//partial down
+	}
+	else{
+		return;
+	}
+	
+	v->x *= -1; 
+
+
+
+
+
+}
+
+void goal_check(ball *b, vector *v){
+	
+	float r = rand()>>30;
+
+	if(b->coordinate->x > 240){
+		
+		b->coordinate->x =120;
+		b->coordinate->y =(float)((rand()%100)+20);
+	
+		if(r==0){
+			v->y = 1;
+		}
+		else{
+			v->y = -1;
+		}
+		
+		obj_set_pos(b->sprite,b->coordinate->x,b->coordinate->y);
+		print("\nPLAYER SCORED\n");
+		return;
+	}
+	
+	if(b->coordinate->x < 0){
+		
+	
+		b->coordinate->x =120;
+		b->coordinate->y =(float)((rand()%80)+30);
+	
+
+		if(r==0){
+			v->y = 1;
+		}
+		else{
+			v->y = -1;
+		}
+			
+		obj_set_pos(b->sprite,b->coordinate->x,b->coordinate->y);
+		print("\nAI SCORED\n");
+		return;
+	}
+	
+}
+
+
 void game(){
 	// INIT PLAYER
-	float x = 10;
-	float y = 80;
-	float speed = 0;
+	float x = 10, y = 80;
+	float speed = 0, cap=2;
 	float acc = (float) 4/16;
 	float dec = (float) 1/16;
-	float cap = 2;
 	u32 tid= 0, pb= 0;
 	OBJ_ATTR *sprite= &obj_buffer[0];
-	obj_set_attr(sprite,
-	ATTR0_TALL,				
-	ATTR1_SIZE_8x16,					
-	ATTR2_PALBANK(pb) | tid);		
+	obj_set_attr(sprite,ATTR0_TALL,ATTR1_SIZE_8x16,ATTR2_PALBANK(pb) | tid);		
 	player *p = create_player(x,y,speed,acc,dec,cap,&sprite);
-	obj_set_pos(p->sprite,p->x,p->y);
+	obj_set_pos(p->sprite,p->coordinate->x,p->coordinate->x);
+	
 	// INIT AI
 	x = 222;
-	y = 0;
+	y = 80;
 	cap = 1;
 	tid = 4;
 	pb = 1;
 	sprite = &obj_buffer[1];
-	obj_set_attr(sprite,
-	ATTR0_TALL,				
-	ATTR1_SIZE_8x16,					
-	ATTR2_PALBANK(pb) | tid);
+	obj_set_attr(sprite,ATTR0_TALL,ATTR1_SIZE_8x16,ATTR2_PALBANK(pb) | tid);
 	player *a = create_player(x,y,speed,acc,dec,cap,&sprite);
-	obj_set_pos(a->sprite,a->x,a->y);
-
-	int b_x= 120, b_y= 80;
+	obj_set_pos(a->sprite,a->coordinate->x,a->coordinate->y);
 	
-	int b_speed_x=-2;
-	float b_speed=0;
-
-	u32 b_tid= 6, b_pb= 2;
-	OBJ_ATTR *ball= &obj_buffer[2];
+	// INIT BALL
+	x= 120, y= 80;
+	speed=0;
+	tid= 6, pb= 2;
+	sprite = &obj_buffer[2];
+	obj_set_attr(sprite,ATTR0_SQUARE,ATTR1_SIZE_8,ATTR2_PALBANK(pb) | tid);		
+	ball *b = create_ball(x,y,&sprite);
+	obj_set_pos(b->sprite,b->coordinate->x,b->coordinate->y);
 	
-		
-		obj_set_attr(ball,
-		ATTR0_SQUARE,				
-		ATTR1_SIZE_8,					
-		ATTR2_PALBANK(b_pb) | b_tid);		
-		obj_set_pos(ball,b_x,b_y);
-		oam_copy(oam_mem, obj_buffer, 3);	
-		
+	vector *ball_speed = create_vector(-2,1);
+
+	oam_copy(oam_mem, obj_buffer, 3);
+	
 	while(1){
 		VBlankIntrWait();
 		key_poll();
-		move_player(p);
+		move_human(p);
+		update_corners_player(p);
+		move_ball(b,ball_speed);
+		update_corners_ball(b);
+		move_ai(a,b);
+		update_corners_player(a);
+		check_collision_human(b,p,ball_speed);
+		check_collision_ai(b,a,ball_speed);
+		goal_check(b,ball_speed);
+		
 		
 		oam_copy(oam_mem, obj_buffer, 3);
 	}
@@ -139,10 +307,10 @@ int main(void){
 
 	oam_init(obj_buffer, 128);
 
-	REG_DISPCNT =  DCNT_OBJ | DCNT_OBJ_1D;// 0001000001000000
+	REG_DISPCNT =  DCNT_OBJ | DCNT_OBJ_1D|DCNT_MODE2;// 0001000001000000
 
 	init_sprites();
-	
+	srand(time(NULL));
 
 	game();
 
