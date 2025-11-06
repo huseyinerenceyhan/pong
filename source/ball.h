8ea24063cce1c97722647c92408fd140c4b5f8e2
huseyinerenceyhan <120252566+huseyinerenceyhan@users.noreply.github.com>
2025-11-06 17:01:42 +0300

    Update ball.h

--- source/ball.h
+++ source/ball.h
@@ -1,40 +1,23 @@
 #ifndef BALL_H
 #define BALL_H
-//#include <tonc.lib>
+#include "point.h"
+#include "vector.h"
 
 typedef struct{
 	
-float x;
-float y;
-float center;
-int radius;
+point *coordinate;
+point corners[4];
 OBJ_ATTR *sprite;
 
-
-	
-} ball;
+}ALIGN(4)ball;
 
 
-ball create_ball(float x, float y,int radius,OBJ_ATTR **sprite){
-	ball *retval = malloc(sizeof(retval));
-	retval->x = x;
-	retval->y = y;
-	retval->radius = radius;
-	retval->center = (float)((x+y)/2)
-	retval->sprite = *sprite;
-	return retval;
-}
+ball *create_ball(float x, float y, OBJ_ATTR **sprite);
 
-void destroy_ball(ball **b){
-	if(ball==NULL){
-		return NULL;
-	}
-	
-	free(*player);
-	*player = NULL;
-	
-	
-}
+void move_ball(ball *b, vector *v);
+void update_corners_ball(ball *b);
+
+void destroy_ball(ball **ball);
 
 
 
