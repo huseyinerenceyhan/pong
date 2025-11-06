d2f26a084b6b4a25da1ea3c711879c8198bbb858
huseyinerenceyhan <120252566+huseyinerenceyhan@users.noreply.github.com>
2025-11-06 17:02:05 +0300

    Update player.c

--- source/player.c
+++ source/player.c
@@ -1,6 +1,7 @@
 #include <tonc.h>
 #include <stdlib.h>
 #include "player.h"
+#include "point.h"
 #include "printdebug.h"
 
 
@@ -12,24 +13,39 @@ player *create_player(float x, float y, float speed, float speed_acc, float spee
 		print("ERROR in create_player()");
 		return NULL;
 	}
-	
-	retval->x = x;
-	retval->y = y;
+	retval->coordinate = create_point(x,y);
 	retval->speed = speed;
 	retval->acc = speed_acc;
 	retval->dec = speed_dec;
 	retval->cap = speed_cap;
 	retval->sprite = *sprite;
+
+	retval->corners[TOP_LEFT] = *create_point(x,y);//top left
+	retval->corners[BOTTOM_LEFT] = *create_point(x,y+16);//bottom left
+	retval->corners[TOP_RIGHT] = *create_point(x+8,y);//top right
+	retval->corners[BOTTOM_RIGHT] = *create_point(x+8,y+16);//bottom right
+	
+
 	retval->score = 0;
 	
 	return retval;
 };
 
+void update_corners_player(player *p){
+	p->corners[TOP_LEFT].y = p->coordinate->y;
+	p->corners[BOTTOM_LEFT].y = p->coordinate->y+16;
+	p->corners[TOP_RIGHT].y = p->coordinate->y;
+	p->corners[BOTTOM_RIGHT].y = p->coordinate->y+16;
+	
+
+	
+}
+
 void destroy_player(player **player){
 	if(*player == NULL){
 		return;
 	}
-	
+	destroy_point(&((*player)->coordinate));
 	free(*player);
 	*player = NULL;
 	
