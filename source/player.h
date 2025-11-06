6cda4cda8018250f9276d8e701a97a347118df25
huseyinerenceyhan <120252566+huseyinerenceyhan@users.noreply.github.com>
2025-11-06 17:02:26 +0300

    Update player.h

--- source/player.h
+++ source/player.h
@@ -1,25 +1,28 @@
 #ifndef PLAYER_H
 #define PLAYER_H
-#include <tonc.h>
-
-
+#include "point.h"
 
 typedef struct{
 
-float x;
-float y;
+point *coordinate;
+point *collidable_top;
+
 float speed;
 float acc;
 float dec;
 float cap;
 int score;
 
-OBJ_ATTR *sprite; 
-
-
-}player;
+OBJ_ATTR *sprite;
+point corners[4];
+}ALIGN(4) player;
 
 player *create_player(float x, float y, float speed, float speed_acc, float speed_dec, float speed_cap, OBJ_ATTR **sprite);
+
+void update_collision_points(player *p);
+
+
+void update_corners_player(player *b);
 
 void destroy_player(player **player);
 
